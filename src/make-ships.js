7eff109b1c4040a32be64404b351feedf750405d
Jameslo599 <jameslo599@gmail.com>
2021-07-30 01:45:30 -0500

    gameboard can now check if all ships are sunk

--- src/make-ships.js
+++ src/make-ships.js
@@ -5,97 +5,107 @@ const Ship = (length) => {
   const hit = (location) => healthBar.splice(location, 1, "hit");
   const isSunk = () => {
     if (healthBar.every((e) => e === "hit")) {
-      return (sunk = true);
-    } else {
-      return (sunk = false);
+      sunk = true;
+      return sunk;
     }
+    sunk = false;
+    return sunk;
   };
   return { healthBar, hit, isSunk };
 };
 
 // Generates gameboards to place ships based on coordinates
 const Gameboard = () => {
-	const carrier = Ship(5);
-	const battleship = Ship(4);
-	const destroyer = Ship(3);
-	const submarine = Ship(3);
-	const patrolBoat = Ship(2);
-	let grid = [
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[]
-	];
-	for (let i = 1; i <= 100; i += 1) {
-		if (i <= 10) {
-			grid[0].push(i);
-		} else if (i <= 20 && i > 10) {
-			grid[1].push(i);
-		} else if (i <= 30 && i > 20) {
-			grid[2].push(i);
-		} else if (i <= 40 && i > 30) {
-			grid[3].push(i);
-		} else if (i <= 50 && i > 40) {
-			grid[4].push(i);
-		} else if (i <= 60 && i > 50) {
-			grid[5].push(i);
-		} else if (i <= 70 && i > 60) {
-			grid[6].push(i);
-		} else if (i <= 80 && i > 70) {
-			grid[7].push(i);
-		} else if (i <= 90 && i > 80) {
-			grid[8].push(i);
-		} else if (i <= 100 && i > 90) {
-			grid[9].push(i);
-		}
-	}
-	const rows = grid[0].length;
-	const columns = grid.length;
-	const getXY = (cell) => {
-		for (let y = 0; y < rows; y += 1) {
-			for ( let x = 0; x < columns; x += 1) {
-				if (grid[y][x] === cell) {
-					return ({x, y})
-				}
-			}
-		}
-	}
+  const lifeBar = ["sunk", "sunk", "sunk", "sunk"];
+  const grid = [[], [], [], [], [], [], [], [], [], []];
+  for (let i = 1; i <= 100; i += 1) {
+    if (i <= 10) {
+      grid[0].push(i);
+    } else if (i <= 20 && i > 10) {
+      grid[1].push(i);
+    } else if (i <= 30 && i > 20) {
+      grid[2].push(i);
+    } else if (i <= 40 && i > 30) {
+      grid[3].push(i);
+    } else if (i <= 50 && i > 40) {
+      grid[4].push(i);
+    } else if (i <= 60 && i > 50) {
+      grid[5].push(i);
+    } else if (i <= 70 && i > 60) {
+      grid[6].push(i);
+    } else if (i <= 80 && i > 70) {
+      grid[7].push(i);
+    } else if (i <= 90 && i > 80) {
+      grid[8].push(i);
+    } else if (i <= 100 && i > 90) {
+      grid[9].push(i);
+    }
+  }
+  const rows = grid[0].length;
+  const columns = grid.length;
+  const getXY = (cell) => {
+    for (let y = 0; y < rows; y += 1) {
+      for (let x = 0; x < columns; x += 1) {
+        if (grid[y][x] === cell) {
+          return { x, y };
+        }
+      }
+    }
+    return null;
+  };
 
-	const isObject = (obj) => obj === Object(obj);
+  const isObject = (obj) => obj === Object(obj);
 
-	const placeShip = (length, cell) => {
-		const coordinates = getXY(cell);
-		grid[coordinates.y].splice(coordinates.x, 1, Ship(length));
-		return grid[coordinates.y][coordinates.x];
-	}
+  const checkVictory = () => {
+    let message = "Not Yet";
+    if (lifeBar.length === 5) {
+      message = "You Win!";
+      return message;
+    }
+    return message;
+  };
 
-	const receiveAttack = (cell) => {
-		const coordinates = getXY(cell);
-		if (isObject(grid[coordinates.y][coordinates.x]) === true) {
-			grid[coordinates.y][coordinates.x].hit(0)
-			return grid[coordinates.y][coordinates.x].isSunk();
-		} else {
-			return 'Miss';
-		}
-	}
+  const placeShip = (length, cell) => {
+    const coordinates = getXY(cell);
+    const ship = Ship(length);
+    for (let i = 0; i < length; i += 1) {
+      grid[coordinates.y].splice(coordinates.x + i, 1, ship);
+    }
+    return grid[coordinates.y][coordinates.x];
+  };
 
-	const mock = (length, cell) => {
-		const coordinates = getXY(cell);
-		grid[coordinates.y].splice(coordinates.x, 1, Ship(length));
-		if (isObject(grid[coordinates.y][coordinates.x]) === true) {
-			grid[coordinates.y][coordinates.x].hit(0)
-			return grid[coordinates.y][coordinates.x].healthBar[0];
-		} else {
-			return 'Number';
-		}
-	}
-	return {grid, getXY, placeShip, receiveAttack, mock}
-}
+  const receiveAttack = (cell) => {
+    const coordinates = getXY(cell);
+    if (isObject(grid[coordinates.y][coordinates.x]) === true) {
+      grid[coordinates.y][coordinates.x].hit(0);
+      if (grid[coordinates.y][coordinates.x].isSunk() === true) {
+        lifeBar.push("sunk");
+      }
+      checkVictory();
+      return grid[coordinates.y][coordinates.x].isSunk();
+    }
+    return "Miss";
+  };
 
-export {Ship, Gameboard};
+  const mock = (length, cell) => {
+    const coordinates = getXY(cell);
+    const ship = Ship(length);
+    for (let i = 0; i < length; i += 1) {
+      grid[coordinates.y].splice(coordinates.x + i, 1, ship);
+    }
+    if (isObject(grid[coordinates.y][coordinates.x]) === true) {
+      grid[coordinates.y][coordinates.x].hit(0);
+      grid[coordinates.y][4].hit(1);
+      grid[coordinates.y][5].hit(2);
+      if (grid[coordinates.y][coordinates.x].isSunk() === true) {
+        lifeBar.push("sunk");
+      }
+      checkVictory();
+      return grid[coordinates.y][coordinates.x].healthBar;
+    }
+    return "Number";
+  };
+  return { grid, getXY, placeShip, receiveAttack, mock };
+};
+
+export { Ship, Gameboard };
--- tests/make-ships.test.js
+++ tests/make-ships.test.js
@@ -1,4 +1,4 @@
-import {Ship, Gameboard} from "../src/make-ships";
+import { Ship, Gameboard } from "../src/make-ships";
 
 test("validate Ship object", () => {
   expect(Ship(4).hit(4)).toStrictEqual(expect.any(Array));
@@ -9,18 +9,27 @@ test("validate Ship object", () => {
 });
 
 test("validate Gameboard object", () => {
-	expect(Gameboard()).toEqual({grid: expect.any(Array), getXY: expect.any(Function), placeShip: expect.any(Function),
-	receiveAttack: expect.any(Function), mock: expect.any(Function)})
+  expect(Gameboard()).toEqual({
+    grid: expect.any(Array),
+    getXY: expect.any(Function),
+    placeShip: expect.any(Function),
+    receiveAttack: expect.any(Function),
+    mock: expect.any(Function),
+  });
 });
 
 test("retrieves Gameboard coordinates", () => {
-	expect(Gameboard().getXY(1)).toEqual({x:0, y:0})
+  expect(Gameboard().getXY(1)).toEqual({ x: 0, y: 0 });
 });
 
 test("places ship based on coordinates", () => {
-	expect(Gameboard().placeShip(3, 16)).toEqual({healthBar: expect.any(Array), hit: expect.any(Function), isSunk: expect.any(Function)})
+  expect(Gameboard().placeShip(3, 16)).toEqual({
+    healthBar: expect.any(Array),
+    hit: expect.any(Function),
+    isSunk: expect.any(Function),
+  });
 });
 
 test("retrieves Gameboard coordinates", () => {
-	expect(Gameboard().mock(3, 4)).toEqual('hit');
+  expect(Gameboard().mock(3, 4)).toEqual(["hit", "hit", "hit"]);
 });
