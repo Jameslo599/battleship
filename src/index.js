e8d0fea30cc74429f7344872a48bf49bdd07b6a5
Jameslo599 <jameslo599@gmail.com>
2021-08-07 00:09:21 -0500

    added copyright bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import makeNavbar from "./navbar";
 import makeDisplay from "./game-display";
+import makeCopyright from "./copyrightbar";
 
 (function playBattleship() {
   makeNavbar();
   makeDisplay();
+  makeCopyright();
 })();
